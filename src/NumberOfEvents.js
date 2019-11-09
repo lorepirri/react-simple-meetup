010569b41375870d6eec099a8c22104e3bba2035
Lorenzo <6698585+lorepirri@users.noreply.github.com>
2019-11-09 21:25:08 +0100

    4.7 set an alert with OOP

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 const DEFAULT_NUMBER_OF_EVENTS = 32
 class NumberOfEvents  extends Component {
@@ -9,7 +10,8 @@ class NumberOfEvents  extends Component {
 
     // init state
     this.state = {
-      numberOfEvents: DEFAULT_NUMBER_OF_EVENTS
+      numberOfEvents: DEFAULT_NUMBER_OF_EVENTS,
+      errorText: ''
     }
   }
 
@@ -17,15 +19,23 @@ class NumberOfEvents  extends Component {
     const value = event.target.value;
     this.setState({ numberOfEvents: value });
     this.props.updateEvents(null, null, value);
+    console.log(parseInt(value));
+    let intValue = parseInt(value);
+    if (isNaN(intValue) || intValue < 1) {
+      this.setState({ errorText: 'Number should be at least 1' });
+    } else {
+      this.setState({ errorText: '' });
+    }    
   }
 
   render() {
     let { numberOfEvents } = this.state;
-    if (!numberOfEvents) {
+    if (numberOfEvents === undefined) {
       numberOfEvents = DEFAULT_NUMBER_OF_EVENTS;
     }
     return (
       <div>
+        <ErrorAlert text={this.state.errorText} />
         <input type="text"
           className="numberOfEvents"
           value={numberOfEvents}
