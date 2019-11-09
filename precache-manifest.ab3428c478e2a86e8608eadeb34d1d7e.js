0d4d5d5274524c3d79d1bf1193d5d80e0e36212c
Lorenzo <6698585+lorepirri@users.noreply.github.com>
2019-11-09 22:06:36 +0100

    Updates

--- precache-manifest.ab3428c478e2a86e8608eadeb34d1d7e.js
+++ precache-manifest.ab3428c478e2a86e8608eadeb34d1d7e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "75505d66dc12fb99e8b00c305d527e5c",
+    "revision": "fde51ffee63b533eb4630daaa6a8e594",
     "url": "/react-simple-meetup/index.html"
   },
   {
-    "revision": "6bb57b8b9a836aefd864",
+    "revision": "a0de4932e11d0791c15f",
     "url": "/react-simple-meetup/static/css/main.f4f6c506.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-simple-meetup/static/js/2.1620e438.chunk.js"
   },
   {
-    "revision": "6bb57b8b9a836aefd864",
-    "url": "/react-simple-meetup/static/js/main.ab7f4e72.chunk.js"
+    "revision": "a0de4932e11d0791c15f",
+    "url": "/react-simple-meetup/static/js/main.37e206dd.chunk.js"
   },
   {
     "revision": "cd3665cbbe21db0f35dd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-simple-meetup/precache-manifest.8838de769b707dd4fd8962de575e1683.js"
+  "/react-simple-meetup/precache-manifest.ab3428c478e2a86e8608eadeb34d1d7e.js"
 );
 
 self.addEventListener('message', (event) => {
