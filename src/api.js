56707e75b76414bbe7f36edb2cacf4cc3ab413d6
Lorenzo <6698585+lorepirri@users.noreply.github.com>
2019-11-09 21:12:51 +0100

    fix suggestions on localhost

--- src/api.js
+++ src/api.js
@@ -62,27 +62,31 @@ async function getAccessToken() {
 
 async function getSuggestions(query) {
   if (window.location.href.startsWith('http://localhost')) {
-    return [
-      {
-        city: 'Munich',
-        country: 'de',
-        localized_country_name: 'Germany',
-        name_string: 'Munich, Germany',
-        zip: 'meetup3',
-        lat: 48.14,
-        lon: 11.58
-      },
-      {
-        city: 'Munich',
-        country: 'us',
-        localized_country_name: 'USA',
-        state: 'ND',
-        name_string: 'Munich, North Dakota, USA',
-        zip: '58352',
-        lat: 48.66,
-        lon: -98.85
-      }
-    ];
+    if (query <= 'Munich') {
+      return [
+        {
+          city: 'Munich',
+          country: 'de',
+          localized_country_name: 'Germany',
+          name_string: 'Munich, Germany',
+          zip: 'meetup3',
+          lat: 48.14,
+          lon: 11.58
+        },
+        {
+          city: 'Munich',
+          country: 'us',
+          localized_country_name: 'USA',
+          state: 'ND',
+          name_string: 'Munich, North Dakota, USA',
+          zip: '58352',
+          lat: 48.66,
+          lon: -98.85
+        }
+      ];
+    } else {
+      return [];
+    }
   }
   
   const token = await getAccessToken();
