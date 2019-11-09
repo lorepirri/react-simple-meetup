e5aea4cff162f6cbc43fc655ed97d79c0dade126
Lorenzo <6698585+lorepirri@users.noreply.github.com>
2019-11-09 17:01:24 +0100

    Updates

---
+++ precache-manifest.ebb7c6e00391732048db2e1ee9151cc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6dd2801e58814df8d841109a0de8ac5",
+    "url": "/react-simple-meetup/index.html"
+  },
+  {
+    "revision": "0403e10f71fbe2e25253",
+    "url": "/react-simple-meetup/static/css/main.92a04015.chunk.css"
+  },
+  {
+    "revision": "4446ff14c3980ca3cbee",
+    "url": "/react-simple-meetup/static/js/2.5df3ddf8.chunk.js"
+  },
+  {
+    "revision": "0403e10f71fbe2e25253",
+    "url": "/react-simple-meetup/static/js/main.52c56587.chunk.js"
+  },
+  {
+    "revision": "cd3665cbbe21db0f35dd",
+    "url": "/react-simple-meetup/static/js/runtime-main.acc87f8c.js"
+  }
+]);
